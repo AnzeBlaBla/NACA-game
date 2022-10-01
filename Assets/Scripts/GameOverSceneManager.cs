0b0cfa33e68b3940c103bb0f3242869b09d0385f
Urban Krepel <urban.kreppel@gmail.com>
2022-10-01 19:26:16 +0200

    Update game over scene

--- Assets/Scripts/GameOverSceneManager.cs
+++ Assets/Scripts/GameOverSceneManager.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class GameOverSceneManager : MonoBehaviour
 {
     public GameObject background;
+    public AnimationClip backgroundAnimation;
+    Animation animation;
+
+    public GameObject filmCanvas;
+    public GameObject endGameCanvas;
 
     private IEnumerator WaitForAnimation ( Animation animation )
-{
-    do
     {
-        yield return null;
-    } while ( animation.isPlaying );
-}
+        do
+        {
+            yield return null;
+        } while ( animation.isPlaying );
+    }
+
+    private IEnumerator runAnimation ( Animation animation )
+    {
+        animation.AddClip( backgroundAnimation, "backgroundAnimation" );
+        animation.Play( "backgroundAnimation" );
+        yield return StartCoroutine( WaitForAnimation( animation ) );
+        filmCanvas.SetActive( false );
+        endGameCanvas.SetActive( true );
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        background.GetComponent<Animation>().Play();
-        StartCoroutine ( WaitForAnimation ( background.GetComponent<Animation>() ) );
+        filmCanvas.SetActive( true );
+        endGameCanvas.SetActive( false );
+        animation = background.GetComponent<Animation>();
+        StartCoroutine(runAnimation(background.GetComponent<Animation>()));
     }
 }
