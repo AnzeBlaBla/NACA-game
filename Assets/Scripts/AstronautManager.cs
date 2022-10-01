188b48f8d7e19b1591393daf34ee1183f837eba4
AnzeBlaBla <anzemajblabla@gmail.com>
2022-10-01 11:20:10 +0200

    Created screens

--- Assets/Scripts/AstronautManager.cs
+++ Assets/Scripts/AstronautManager.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
 
 public class AstronautManager : MonoBehaviour
 {
-    public float hunger = 100;
-    public float energy = 100;
-    public float happiness = 100;
+    private static string playerPrefsKey = "astronaut_stats";
     
-    
+    public float food = 100;
+    public float water = 100;
+    public float fitness = 100;
+
+    public void SaveStats()
+    {
+        // save to playerprefs
+        PlayerPrefs.SetString(playerPrefsKey, JsonConvert.SerializeObject(this));
+    }
+
+    public void LoadStats()
+    {
+        // load from playerprefs
+        if (PlayerPrefs.HasKey(playerPrefsKey))
+        {
+            JsonConvert.PopulateObject(PlayerPrefs.GetString(playerPrefsKey), this);
+        }
+    }
 }
