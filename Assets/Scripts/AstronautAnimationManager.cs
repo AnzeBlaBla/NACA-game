0d8ad34f48235e438d13b0f1da642bfa8bebc789
Urban Krepel <urban.kreppel@gmail.com>
2022-10-01 22:09:01 +0200

    Air lock prevent spaming

--- Assets/Scripts/AstronautAnimationManager.cs
+++ Assets/Scripts/AstronautAnimationManager.cs
@@ -6,4 +6,25 @@ public class AstronautAnimationManager : MonoBehaviour
 {
 
     public Animation astronautAnimation;
+
+    private bool isPlayingAnimation = false;
+    public IEnumerator PlayAnimation(string animationName)
+    {
+        if (isPlayingAnimation)
+        {
+            yield break;
+        }
+        isPlayingAnimation = true;
+        astronautAnimation.Play(animationName);
+        yield return StartCoroutine( WaitForAnimation( astronautAnimation ) );
+        isPlayingAnimation = false;
+    }
+
+    private IEnumerator WaitForAnimation ( Animation animation )
+    {
+        do
+        {
+            yield return null;
+        } while ( animation.isPlaying );
+    }
 }
