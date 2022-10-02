82d180cdd593cfd2cf0253d81a8bbc283444586e
Urban Krepel <urban.kreppel@gmail.com>
2022-10-02 09:52:08 +0200

    Update outside air lock

--- Assets/Scripts/OutsideAirLockScreen/OutsideAirLockDoor.cs
+++ Assets/Scripts/OutsideAirLockScreen/OutsideAirLockDoor.cs
@@ -4,22 +4,66 @@ using UnityEngine;
 
 public class OutsideAirLockDoor : MonoBehaviour
 {
+    ClickableObject clickableObject;
     private Animator animator;
 
+    private bool isAnimating = false;
    void Awake()
     {
+        clickableObject = GetComponent<ClickableObject>();
+        clickableObject.onClick += OnClick;
+
         animator = GetComponent<Animator>();
     }
 
     void Start(){
         ScreenManager.Instance.DisableButtons();
-        StartCoroutine(this.openDoor());
+        StartCoroutine(this.openDoor(true));
     }
 
-    IEnumerator openDoor(){
-        animator.SetTrigger("TrOpenAirLock");
-        yield return new WaitForSeconds(4);
+    IEnumerator openDoor(bool goingOut){
+        if(!isAnimating)
+        {
+            isAnimating = true;
+            AstronautManager.Instance.gameObject.GetComponent<AstronautAnimationManager>().lockAstronaut();
+            animator.SetTrigger("TrOpenAirLock");
+            yield return new WaitForSeconds(4);
+            yield return StartCoroutine(goingOut ? goOut() : goIn());
+            if(goingOut){
+                animator.SetTrigger("TrCloseAirLock");
+                changeRendererLayout(0);
+                yield return new WaitForSeconds(4);
+            }
+            isAnimating = false;
+        }
+    }
+
+    IEnumerator goIn(){
+        yield return StartCoroutine(AstronautManager.Instance.gameObject.GetComponent<AstronautAnimationManager>().PlayAnimation("minimizing"));
+    }
+
+    IEnumerator goOut(){
         yield return StartCoroutine(AstronautManager.Instance.gameObject.GetComponent<AstronautAnimationManager>().PlayAnimation("maximizing"));
         AstronautManager.Instance.gameObject.GetComponent<AstronautAnimationManager>().unlockAstronaut();
     }
+
+    void changeRendererLayout(int index){
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sortingOrder = index;
+    }
+
+    void OnClick(){
+        if(!isAnimating)
+        {
+            StartCoroutine(goInside());
+        }
+    }
+
+    IEnumerator goInside(){
+        yield return StartCoroutine(openDoor(false));
+        ScreenManager.Instance.EnableButtons();
+        ScreenManager.Instance.LoadScreen("AirLockScreen", Color.black);
+        ScreenManager.Instance.UnloadScreenWithName("OutsideAirLockScreen");
+    }
+
 }
