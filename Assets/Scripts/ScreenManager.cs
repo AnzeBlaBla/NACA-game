b3769db645cd8977012ec6e320fe69d7ed8695ac
Gorazd Kotnik <59970689+gorazdkotnik@users.noreply.github.com>
2022-10-02 08:58:37 +0200

    added sfx for scene switch

--- Assets/Scripts/ScreenManager.cs
+++ Assets/Scripts/ScreenManager.cs
@@ -6,89 +6,92 @@ using UnityEngine.UI;
 
 public class ScreenManager : Singleton<ScreenManager>
 {
-    public GameObject nextButton;
-    public GameObject backButton;
+  public GameObject nextButton;
+  public GameObject backButton;
 
-    public List<ScreenItem> screens;
-    public int currentScreenIndex = 0;
+  public List<ScreenItem> screens;
+  public int currentScreenIndex = 0;
 
-    public Image background;
+  public Image background;
 
-    public void Start()
+  [SerializeField] private GameObject sceneSwitchSound;
+
+  public void Start()
+  {
+    // Unload all screens
+    for (int i = 0; i < screens.Count; i++)
     {
-        // Unload all screens
-        for (int i = 0; i < screens.Count; i++)
-        {
-            UnloadScreen(i);
-        }
-
-        // Load the first screen
-        LoadCurrentScreen();
-        this.UnloadScreenWithName("OutsideAirLockScreen");
+      UnloadScreen(i);
     }
 
+    // Load the first screen
+    LoadCurrentScreen();
+    this.UnloadScreenWithName("OutsideAirLockScreen");
+  }
 
-    public void NextScreen()
+
+  public void NextScreen()
+  {
+    int oldIndex = currentScreenIndex;
+    currentScreenIndex++;
+    if (currentScreenIndex >= screens.Count)
     {
-        int oldIndex = currentScreenIndex;
-        currentScreenIndex++;
-        if (currentScreenIndex >= screens.Count)
-        {
-            currentScreenIndex = 0;
-        }
-        LoadCurrentScreen();
-        UnloadScreen(oldIndex);
+      currentScreenIndex = 0;
     }
+    LoadCurrentScreen();
+    UnloadScreen(oldIndex);
+  }
 
-    public void PreviousScreen()
+  public void PreviousScreen()
+  {
+    int oldIndex = currentScreenIndex;
+    currentScreenIndex--;
+    if (currentScreenIndex < 0)
     {
-        int oldIndex = currentScreenIndex;
-        currentScreenIndex--;
-        if (currentScreenIndex < 0)
-        {
-            currentScreenIndex = screens.Count - 1;
-        }
-        LoadCurrentScreen();
-        UnloadScreen(oldIndex);
+      currentScreenIndex = screens.Count - 1;
     }
+    LoadCurrentScreen();
+    UnloadScreen(oldIndex);
+  }
 
-    public void LoadCurrentScreen()
+  public void LoadCurrentScreen()
+  {
+    LoadScreen(screens[currentScreenIndex].sceneName, screens[currentScreenIndex].backgroundColor);
+    sceneSwitchSound.GetComponent<AudioSource>().Play();
+  }
+
+  public void LoadScreen(string name, Color color)
+  {
+    SceneManager.LoadScene(name, LoadSceneMode.Additive);
+    background.color = color;
+
+    AstronautManager.Instance.gameObject.GetComponent<MovableObject>().ResetPosition();
+
+  }
+
+  public void UnloadScreen(int index)
+  {
+    this.UnloadScreenWithName(screens[index].sceneName);
+  }
+  public void UnloadScreenWithName(string name)
+  {
+    try
     {
-        LoadScreen(screens[currentScreenIndex].sceneName, screens[currentScreenIndex].backgroundColor);
+      SceneManager.UnloadSceneAsync(name);
     }
-    
-    public void LoadScreen(string name, Color color)
-    {
-        SceneManager.LoadScene(name, LoadSceneMode.Additive);
-        background.color = color;
+    catch { }
 
-        AstronautManager.Instance.gameObject.GetComponent<MovableObject>().ResetPosition();
+  }
 
-    }
+  public void EnableButtons()
+  {
+    nextButton.SetActive(true);
+    backButton.SetActive(true);
+  }
 
-    public void UnloadScreen(int index)
-    {
-            this.UnloadScreenWithName(screens[index].sceneName);
-    }
-    public void UnloadScreenWithName(string name)
-    {
-        try
-        {
-            SceneManager.UnloadSceneAsync(name);
-        }
-        catch { }
-
-    }
-
-    public void EnableButtons()
-    {
-        nextButton.SetActive(true);
-        backButton.SetActive(true);
-    }
-
-    public void DisableButtons()
-    {
-        nextButton.SetActive(false);
-        backButton.SetActive(false);
-    }
+  public void DisableButtons()
+  {
+    nextButton.SetActive(false);
+    backButton.SetActive(false);
+  }
 }
