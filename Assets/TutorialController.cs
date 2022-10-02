d80efd392df45be19e6e1a53962695aba29813b3
AnzeBlaBla <anzemajblabla@gmail.com>
2022-10-02 21:29:02 +0200

    Implemented settings

---
+++ Assets/TutorialController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    public string tutorialCanvasId;
+
+    private void Awake()
+    {
+        string playerPrefsKey = "tutorial_" + tutorialCanvasId;
+        if (PlayerPrefs.GetInt(playerPrefsKey, 1) == 1)
+        {
+            gameObject.SetActive(false);
+        }
+
+        PlayerPrefs.SetInt(playerPrefsKey, 1);
+    }
+
+    public void CloseTutorial()
+    {
+        gameObject.SetActive(false);
+    }
+}
