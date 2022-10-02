6e7548bdf544a64a3b0454e0a24d40310c6add5d
Gorazd Kotnik <59970689+gorazdkotnik@users.noreply.github.com>
2022-10-02 09:17:18 +0200

    added toilet flush sfx

--- Assets/Scripts/BathroomScene/BathroomScreenManager.cs
+++ Assets/Scripts/BathroomScene/BathroomScreenManager.cs
@@ -4,52 +4,59 @@ using UnityEngine;
 
 public class BathroomScreenManager : MonoBehaviour
 {
-    public GameObject curtain;
+  public GameObject curtain;
 
-    private bool isInAnimation = false;
+  private bool isInAnimation = false;
 
-    public void Start(){
-        Vector2 position = curtain.GetComponent<RectTransform>().anchoredPosition;
-        position.x = -Screen.width + 20;
-        curtain.GetComponent<RectTransform>().anchoredPosition = position;
-    }
+  [SerializeField] private GameObject toiletFlushSound;
 
-    IEnumerator closeCurtain()
+
+  public void Start()
+  {
+    Vector2 position = curtain.GetComponent<RectTransform>().anchoredPosition;
+    position.x = -Screen.width + 20;
+    curtain.GetComponent<RectTransform>().anchoredPosition = position;
+  }
+
+  IEnumerator closeCurtain()
+  {
+    Vector2 position = curtain.GetComponent<RectTransform>().anchoredPosition;
+
+    while (position.x < 0)
     {
-        Vector2 position = curtain.GetComponent<RectTransform>().anchoredPosition;
- 
-        while (position.x < 0)
-        {
-            position.x += Screen.width / 20;
-            curtain.GetComponent<RectTransform>().anchoredPosition = position;
-            yield return new WaitForSeconds(0.1f);
-        }
-        position.x = 0;
-        curtain.GetComponent<RectTransform>().anchoredPosition = position;
+      position.x += Screen.width / 20;
+      curtain.GetComponent<RectTransform>().anchoredPosition = position;
+      yield return new WaitForSeconds(0.1f);
     }
+    position.x = 0;
+    curtain.GetComponent<RectTransform>().anchoredPosition = position;
+  }
 
-    IEnumerator openCurtain()
+  IEnumerator openCurtain()
+  {
+    Vector2 position = curtain.GetComponent<RectTransform>().anchoredPosition;
+
+    while (position.x > -Screen.width)
     {
-        Vector2 position = curtain.GetComponent<RectTransform>().anchoredPosition;
-
-        while (position.x > -Screen.width)
-        {
-            position.x -= Screen.width / 20;
-            curtain.GetComponent<RectTransform>().anchoredPosition = position;
-            yield return new WaitForSeconds(0.1f);
-        }
-        position.x = -Screen.width + 20;
-        curtain.GetComponent<RectTransform>().anchoredPosition = position;
+      position.x -= Screen.width / 20;
+      curtain.GetComponent<RectTransform>().anchoredPosition = position;
+      yield return new WaitForSeconds(0.1f);
     }
+    position.x = -Screen.width + 20;
+    curtain.GetComponent<RectTransform>().anchoredPosition = position;
+  }
 
-    public IEnumerator closeAndOpenCurtain()
-    {
-        if (isInAnimation)
-            yield break;
-        isInAnimation = true;
-        yield return StartCoroutine(closeCurtain());
-        yield return new WaitForSeconds(3);
-        yield return StartCoroutine(openCurtain());
-        isInAnimation = false;
-    }
+  public IEnumerator closeAndOpenCurtain()
+  {
+    if (isInAnimation)
+      yield break;
+    isInAnimation = true;
+    yield return StartCoroutine(closeCurtain());
+    yield return new WaitForSeconds(3);
+
+    toiletFlushSound.GetComponent<AudioSource>().Play();
+
+    yield return StartCoroutine(openCurtain());
+    isInAnimation = false;
+  }
 }
