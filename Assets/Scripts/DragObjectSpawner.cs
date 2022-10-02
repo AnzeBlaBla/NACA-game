c40009aacf9e3240195ba03b1c82999f6ea9b9a5
Gorazd Kotnik <59970689+gorazdkotnik@users.noreply.github.com>
2022-10-02 09:04:41 +0200

    added eat and drink sfx

--- Assets/Scripts/DragObjectSpawner.cs
+++ Assets/Scripts/DragObjectSpawner.cs
@@ -6,66 +6,70 @@ using UnityEngine.InputSystem;
 
 public class DragObjectSpawner : MonoBehaviour
 {
-    GameInputs inputs;
+  GameInputs inputs;
 
-    GameObject held;
+  GameObject held;
 
-    public GameObject toSpawn;
-    public string useItem;
+  public GameObject toSpawn;
+  public string useItem;
 
-    void Start()
+  [SerializeField] private GameObject draggableObjectSound;
+
+  void Start()
+  {
+
+    inputs = new GameInputs();
+    inputs.Interaction.Enable();
+    inputs.Interaction.Tap.performed += ctx => SpawnObject(ctx);
+    inputs.Interaction.Tap.canceled += ctx => StopHold(ctx);
+
+  }
+
+  void SpawnObject(InputAction.CallbackContext ctx)
+  {
+    Vector2 mousePos = PointerPos();
+
+    // get object under mouse
+    RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+    // if hit this or child
+    if (hit.collider != null && (hit.collider.gameObject == gameObject || hit.collider.gameObject.transform.IsChildOf(transform)))
     {
+      if (StorageManager.Instance.GetData(useItem) > 0)
+      {
+        held = Instantiate(toSpawn, mousePos, Quaternion.identity);
+        StorageManager.Instance.ChangeData(useItem, -1f);
 
-        inputs = new GameInputs();
-        inputs.Interaction.Enable();
-        inputs.Interaction.Tap.performed += ctx => SpawnObject(ctx);
-        inputs.Interaction.Tap.canceled += ctx => StopHold(ctx);
-
+        draggableObjectSound.GetComponent<AudioSource>().Play();
+      }
     }
+  }
 
-    void SpawnObject(InputAction.CallbackContext ctx)
+  void StopHold(InputAction.CallbackContext ctx)
+  {
+    if (held != null)
     {
-        Vector2 mousePos = PointerPos();
+      Destroy(held);
 
-        // get object under mouse
-        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
-
-        // if hit this or child
-        if (hit.collider != null && (hit.collider.gameObject == gameObject || hit.collider.gameObject.transform.IsChildOf(transform)))
-        {
-            if (StorageManager.Instance.GetData(useItem) > 0)
-            {
-                held = Instantiate(toSpawn, mousePos, Quaternion.identity);
-                StorageManager.Instance.ChangeData(useItem, -1f);
-            }
-        }
+      StorageManager.Instance.ChangeData(useItem, 1f);
     }
+  }
 
-    void StopHold(InputAction.CallbackContext ctx)
+  private void Update()
+  {
+    if (held != null)
     {
-        if (held != null)
-        {
-            Destroy(held);
-
-            StorageManager.Instance.ChangeData(useItem, 1f);
-        }
+      held.transform.position = PointerPos();
     }
+  }
 
-    private void Update()
-    {
-        if (held != null)
-        {
-            held.transform.position = PointerPos();
-        }
-    }
+  void OnDestroy()
+  {
+    inputs.Interaction.Disable();
+  }
 
-    void OnDestroy()
-    {
-        inputs.Interaction.Disable();
-    }
-
-    Vector2 PointerPos()
-    {
-        return Camera.main.ScreenToWorldPoint(inputs.Interaction.PointerPosition.ReadValue<Vector2>());
-    }
+  Vector2 PointerPos()
+  {
+    return Camera.main.ScreenToWorldPoint(inputs.Interaction.PointerPosition.ReadValue<Vector2>());
+  }
 }
